6880af4bafe41adc9d633f3655e89ea17a554fdd
Breno Araujo <breno.bal.araujo@gmail.com>
2020-06-05 11:12:51 -0300

    configurando evento de reinício de jogo

--- ticTacToe/script.js
+++ ticTacToe/script.js
@@ -1,21 +1,19 @@
 const game = {
-    counter: 0,
     cells: document.querySelectorAll(".cell"),
     positions: ["","","","","","","","",""],
     symbols: ["o", "x"],
     winningSequences: [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
     symbolFlag: 0,
+    runningFlag: false,
 
 
     makePlay: function(e) {
         game.positions[e.getAttribute("id")] = game.symbols[game.symbolFlag];
-        game.counter++;
         game.checkWinner(e.getAttribute("id"));
     },
 
     checkWinner: function(index) {
         let actualSymbol = game.symbols[game.symbolFlag];
-        let wins;
         for(let i of game.winningSequences) {
             for(let j=0; j<3; j++) {
                 if(i[j]==index) {
@@ -35,14 +33,23 @@ const game = {
                 game.cells[ar[i][0]].style.color = "#FFD700";
                 game.cells[ar[i][1]].style.color = "#FFD700";
                 game.cells[ar[i][2]].style.color = "#FFD700";
+                game.winFlag = true;
                 game.removeAllEvents();
             }
         }
     },
 
     change: function() {
-        if(game.symbolFlag == 1)    return 0;
-        else                        return 1;
+        if(game.symbolFlag == 1) {
+            document.querySelectorAll(".player")[0].className = "player active";
+            document.querySelectorAll(".player")[1].className = "player";
+            return 0;
+        } else {
+            document.querySelectorAll(".player")[1].className = "player active";
+            document.querySelectorAll(".player")[0].className = "player";
+            return 1;
+        }
+            
     },
 
     draw: function(e) {
@@ -53,25 +60,33 @@ const game = {
     },
 
     init: function() {
-        // game.winningSequences = game.fillWins();
-        document.querySelector(".regame").addEventListener("click", game.fillBlank);
+        game.positions= ["","","","","","","","",""];
+        game.winningSequences = game.fillWins();
+        game.cells.forEach((el) => {
+            el.style.color = "white";
+        });
         game.addBoardEvents();
+        if(game.runningFlag == false) {
+            game.runningFlag = true;
+            document.querySelector(".regame").addEventListener("click", () => {
+                for(i of game.cells) {
+                    i.innerHTML = "";
+                }
+                document.querySelectorAll(".player")[0].className = "player active";
+                document.querySelectorAll(".player")[1].className = "player";
+                game.init();
+            });
+        }
     },
 
     addBoardEvents: function() {
+        game.symbolFlag = 0;
         let i = 0;
         for(let el of this.cells) {
             el.addEventListener("click", game.draw);
             el.setAttribute("id", i);
             i++;
         }
-    },
-
-    fillBlank: function() {
-        for(i of game.cells) {
-            i.innerHTML = "";
-        }
-        game.addBoardEvents();
     },
 
     fillWins: function() {
