063a0829fbed16aba1d2609811c4ab631dcec1d1
breno araujo <breno.bal.araujo@gmail.com>
2020-07-24 15:23:12 -0300

    destructuring

---
+++ fundamentals/28-Destructuring2.js
@@ -0,0 +1,11 @@
+const [a] = [10]
+console.log(a)
+
+const [n1, n2, n3, n4] = [10,7,9,8]
+console.log(n1)
+console.log(n2)
+console.log(n3)
+console.log(n4)
+
+const [,[, nota]] = [[,8,9],[9,6,8]]
+console.log(nota)
